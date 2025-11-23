e373a9ecffcf5dadfce905431fa1b9237b223b4e
atenfyr <atenefyr@gmail.com>
2025-11-23 05:05:35 -0600

    batch file to generate pak for aml-classic, light docs

---
+++ generate_pak.bat
@@ -0,0 +1,15 @@
+@echo off
+WHERE repak >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO repak.exe not found in PATH or current working directory; cannot proceed
+    goto :done
+)
+
+del /S /Q "./pak/UE4SS"
+xcopy /s /e /y "./out/mod" "./pak/UE4SS"
+repak pack --version V4 --compression Zlib "./pak" "./out/000-AutoIntegratorForAML-1.0.0_P.pak"
+echo.
+echo All done!
+
+:done
+pause

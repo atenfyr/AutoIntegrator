45cf494d4006a2e21a05c8415157ad385d6a1a55
atenfyr <atenefyr@gmail.com>
2025-11-23 00:54:19 -0600

    initial commit

---
+++ AutoIntegrator/dllmain.cpp
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <windows.h>
+#include <fstream>
+#include <UE4SSProgram.hpp>
+#include <Mod/CppUserModBase.hpp>
+#include <DynamicOutput/DynamicOutput.hpp>
+
+using namespace RC;
+
+// THE FOLLOWING METHODS ARE ADAPTED FROM STACK OVERFLOW ANSWERS!
+// THEY ARE NOT LICENSED UNDER MIT
+
+// CC BY-SA 2.5 (https://creativecommons.org/licenses/by-sa/2.5/se/deed.en)
+// This code is copyrighted by StackOverflow user "waqas" https://stackoverflow.com/users/58008/waqas
+// Minor changes were made to this source code from the original. No warranties are given. See the original license text for more information.
+// https://stackoverflow.com/a/478960
+std::string AutoIntegrator_exec(std::string cmd_cpp) {
+    const char* cmd = cmd_cpp.c_str();
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&_pclose)> pipe(_popen(cmd, "r"), _pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+// CC BY-SA 2.5 (https://creativecommons.org/licenses/by-sa/2.5/se/deed.en)
+// This code is copyrighted by StackOverflow user "Evan Teran" https://stackoverflow.com/users/13430/evan-teran
+// Minor changes were made to this source code from the original. No warranties are given. See the original license text for more information.
+// https://stackoverflow.com/a/217605
+inline void AutoIntegrator_rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+        }).base(), s.end());
+}
+
+// CC BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/deed.en)
+// This code is copyrighted by StackOverflow user "mkaes" https://stackoverflow.com/users/264338/mkaes
+// Minor changes were made to this source code from the original. No warranties are given. See the original license text for more information.
+// https://stackoverflow.com/a/6924332
+std::string get_dll_path()
+{
+    wchar_t path[MAX_PATH];
+    HMODULE hm = NULL;
+
+    if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+        GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+        (LPCWSTR)&AutoIntegrator_exec, &hm) == 0)
+    {
+        int ret = GetLastError();
+        fprintf(stderr, "GetModuleHandle failed, error = %d\n", ret);
+        return "."; // unlikely to work
+    }
+    if (GetModuleFileNameW(hm, path, sizeof(path)) == 0)
+    {
+        int ret = GetLastError();
+        fprintf(stderr, "GetModuleFileName failed, error = %d\n", ret);
+        return "."; // unlikely to work
+    }
+
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+    std::wstring out_str = path;
+    std::string out_str_narrow = converter.to_bytes(out_str);
+    return out_str_narrow;
+}
+
+// ALL CODE FROM THIS POINT ON IS MIT-LICENSED BY ATENFYR
+// SEE LICENSE.MD FOR MORE INFORMATION
+class AutoIntegrator : public RC::CppUserModBase
+{
+public:
+    std::string ver;
+    std::string folder_path;
+
+    AutoIntegrator() : CppUserModBase()
+    {
+        folder_path = get_dll_path() + "/../..";
+
+        ver = AutoIntegrator_exec(folder_path + "/ModIntegrator.exe version");
+        AutoIntegrator_rtrim(ver);
+
+        std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+        std::wstring ver_wide_cpp = converter.from_bytes(ver);
+        const wchar_t* ver_wide = ver_wide_cpp.c_str();
+
+        ModName = STR("AutoIntegrator");
+        ModVersion = ver_wide;
+        ModDescription = STR("atenfyr's AutoIntegrator, for loading classic AstroModLoader mods through UE4SS");
+        ModAuthors = STR("atenfyr");
+        // Do not change this unless you want to target a UE4SS version
+        // other than the one you're currently building with somehow.
+        //ModIntendedSDKVersion = STR("2.6");
+
+        std::wstring log_out_start = L"Initializing AutoIntegrator v";
+        log_out_start += ver_wide_cpp;
+        log_out_start += L" by atenfyr\n";
+        Output::send<LogLevel::Normal>(log_out_start);
+
+        std::wstring log_out_2 = L"Folder path: ";
+        log_out_2 += converter.from_bytes(folder_path);
+        log_out_2 += L"\n";
+        Output::send<LogLevel::Verbose>(log_out_2);
+
+        Output::send<LogLevel::Normal>(L"Performing integration...\n");
+
+        std::string paksPath;
+        std::ifstream in(folder_path + "/paks_dir.txt", std::ios_base::in);
+        std::getline(in, paksPath);
+        in.close();
+
+        AutoIntegrator_rtrim(paksPath);
+
+        if (paksPath.empty() || paksPath == "default")
+        {
+            // default
+            char* localappdata = getenv("LOCALAPPDATA");
+            std::string localappdata2 = localappdata;
+            paksPath = localappdata2 + "/Astro/Saved/Paks";
+        }
+
+        std::wstring game_exec_dir = UE4SSProgram::get_program().get_game_executable_directory();
+        std::string game_exec_dir_narrow = converter.to_bytes(game_exec_dir);
+
+        std::string finalCmd = folder_path + "/ModIntegrator.exe " + paksPath + " " + game_exec_dir_narrow + "/../../Content/Paks";
+        std::wstring finalCmd_cpp = converter.from_bytes(finalCmd) + L"\n";
+        const wchar_t* finalCmd_wide = finalCmd_cpp.c_str();
+        Output::send<LogLevel::Verbose>(finalCmd_wide);
+
+        std::string integrator_out = AutoIntegrator_exec(finalCmd.c_str());
+        AutoIntegrator_rtrim(integrator_out);
+        integrator_out += "\n";
+        std::wstring integrator_out_cpp = converter.from_bytes(integrator_out);
+        const wchar_t* integrator_out_wide = integrator_out_cpp.c_str();
+        Output::send<LogLevel::Normal>(integrator_out_wide);
+    }
+
+    ~AutoIntegrator() override
+    {
+    }
+
+    auto on_program_start() -> void override
+    {
+
+    }
+
+    auto on_update() -> void override
+    {
+
+    }
+};
+
+#define AUTO_INTEGRATOR_API __declspec(dllexport)
+extern "C"
+{
+    AUTO_INTEGRATOR_API RC::CppUserModBase* start_mod()
+    {
+        return new AutoIntegrator();
+    }
+
+    AUTO_INTEGRATOR_API void uninstall_mod(RC::CppUserModBase* mod)
+    {
+        delete mod;
+    }
+}
